adc0b7e2dab0cc8e3e424b66f26b5e5e204085da
The-Collector <thecollector989@gmail.com>
2015-04-08 10:48:24 -0700

    a

--- CSClassList.cpp
+++ CSClassList.cpp
@@ -1,1 +1,25 @@
+#include "CSClassList.h"
 
+CSClassList::AddClass(const string N_STRING)
+{
+	if(!isFull)
+	{
+		nameAr[index] = N_STRING;
+	}
+}
+
+CSClassList::IsFull() const
+{
+	if(index == AR_SIZE)
+	{
+		isFull = true;
+	}
+	else
+	{
+		isFull = false;
+	}
+
+	return isFull;
+}
+
+	
--- CSClassList.h
+++ CSClassList.h
@@ -1,1 +1,23 @@
+#ifndef CSCLASSLIST_H_
+#define CSCLASSLIST_H_
 
+class CSClassList
+{
+	public:
+		CSClassList();
+		virtual ~CSClassList();
+
+		void AddClass(const string N_STRING);
+		string GetLongestName() const;
+		int GetNumClass() const;
+		string DisplayName(int index) const;
+		bool IsFull() const;
+
+	private:
+		static const int AR_SIZE = 10;
+		int currIndex;
+		string nameAr[AR_SIZE];
+
+};
+
+#endif
